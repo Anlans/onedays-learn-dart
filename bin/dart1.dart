77bce6c29902852e3cc5456511908518362dadfa
honeyShi <918828350@qq.com>
2021-02-28 12:35:03 +0800

    第七课更新

--- bin/dart1.dart
+++ bin/dart1.dart
@@ -1,8 +1,4 @@
-void show(int a, {String name, int age}) {
-  print(a);
-  print(name);
-  print(age);
-}
 void main() {
-  show(12, age:24, name:'aq');
+  var str='AaaBB';
+  print(str.codeUnitAt(0));
 }
